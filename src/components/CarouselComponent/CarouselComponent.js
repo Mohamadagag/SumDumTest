9473f709d04a0910acbb023cd14baac31f154b56
Mohamad Agag <mohamad.agag007@gmail.com>
2023-02-15 15:28:27 +0200

    update carousel pics

--- src/components/CarouselComponent/CarouselComponent.js
+++ src/components/CarouselComponent/CarouselComponent.js
@@ -1,7 +1,8 @@
 import Carousel from "react-bootstrap/Carousel";
 import "./CarouselComponent.css";
-import Slide1 from "../../assets/slide1.jpg";
-import Slide2 from "../../assets/productImages/Slide2.jpg";
+import Slide1 from "../../assets/Slide1.png";
+import Slide2 from "../../assets/Slide2.png";
+import Slide3 from "../../assets/Slide3.png";
 import { Link } from "react-router-dom";
 
 const CarouselComponent = () => {
@@ -10,7 +11,7 @@ const CarouselComponent = () => {
       <Carousel.Item>
         <img className="d-block w-100" src={Slide1} alt="First slide" />
         <Carousel.Caption>
-          <h3 className="carousel-title">Entremets</h3>
+          <h3 className="carousel-title">ENTREMET</h3>
           <p className="carousel-description">
             Full Cakes Available on pre-order. Moist and airy hazelnut sponge
             cake topped with hazelnut crunch.
@@ -22,9 +23,23 @@ const CarouselComponent = () => {
       </Carousel.Item>
 
       <Carousel.Item>
+        <img className="d-block w-100" src={Slide3} alt="First slide" />
+        <Carousel.Caption>
+          <h3 className="carousel-title">VIENNOISERIES</h3>
+          <p className="carousel-description">
+            Madeleines and financiers… the perfect tiny treats you can take with
+            you everywhere, and enjoy at any time of the day.
+          </p>
+          <Link to="/shop" className="carousel-order-btn">
+            ORDER NOW
+          </Link>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
         <img className="d-block w-100" src={Slide2} alt="First slide" />
         <Carousel.Caption>
-          <h3 className="carousel-title">Small Bites</h3>
+          <h3 className="carousel-title">POUNDCAKE</h3>
           <p className="carousel-description">
             Madeleines and financiers… the perfect tiny treats you can take with
             you everywhere, and enjoy at any time of the day.
